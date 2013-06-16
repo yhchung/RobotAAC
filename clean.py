bae800aaf1fd1d0f81243a3dd9108b7ff523bc09
Younghyun Chung <younghyun.chung@gmail.com>
2013-06-15 23:28:11 -0400

    update
    adjust timestamp

--- clean.py
+++ clean.py
@@ -16,18 +16,32 @@ out_file = "../result/RobotAAC-0430.csv"
 rows = list(csv.reader(open(in_file, 'rb'), delimiter='|', skipinitialspace=True))
 
 data=[]
-data.append(["timestamp", "id", "command", "datetime", "elapsed_time"]) 
+data.append(["timestamp", "id", "command", "adjusted_timestamp", "datetime", "elapsed_time"]) 
 
 last_time = int(rows[0][0])
 
 for line in rows:
 #  if line[2] and not line[2] == "Keep Alive":
   if line[2]:
-    line.append(time.strftime('%Y-%m-%d %H:%M:%S', \
-                time.localtime(int(line[0])))) # timestamp
+    if line[1] == '0':
+      line.append(int(line[0])+3)
+    elif line[1] == '1': 
+      line.append(int(line[0]))
+    elif line[1] == '2': 
+      line.append(int(line[0])-1)
+    elif line[1] == '3': 
+      line.append(int(line[0])+1)
+    elif line[1] == '4': 
+      line.append(int(line[0])-180)
+    else:
+      line.append(int(line[0]))
 
-    line.append(int(line[0]) - last_time) # elapsed_time
-    last_time = int(line[0])              # update new last_time
+    line.append(time.strftime('%Y-%m-%d %H:%M:%S', \
+                time.localtime(int(line[3])))) # timestamp
+
+    line.append(int(line[3]) - last_time) # elapsed_time
+    last_time = int(line[3])              # update new last_time
+
 
     data.append(line)
     print(line)
